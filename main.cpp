663baf468f0d7433a5799d3fc51bedad4c79ddff
Rohan Behl <www.rbehl.com>
2019-06-15 22:05:52 -0700

    Added Dielectric mat

--- main.cpp
+++ main.cpp
@@ -12,25 +12,11 @@ void fileIO() {
 	freopen("output.ppm", "w", stdout);
 }
 
-float InvSqrt(float x) {
-	float xhalf = 0.5f * x;
-	int i = *(int *)&x;				// store floating-point bits in integer
-	i = 0x5f3759df - (i >> 1);		// initial guess for Newton's method
-	x = *(float *)&i;				// convert new bits into float
-	x = x * (1.5f - xhalf * x * x); // One round of Newton's method
-	return x;
-}
-
-float Sqrt(float x) {
-	return 1 / InvSqrt(x);
-}
-
-
 vec3 color(const ray &r, hitable *world, int depth) {
 	hit_record rec;
 	if (world->hit(r, 0.001, FLT_MAX, rec)) {
 		ray scattered;
-		vec3 attenuation;
+		vec3 attenuation;  
 		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
 			return attenuation * color(scattered, world, depth + 1);
 		} else {
@@ -45,14 +31,17 @@ vec3 color(const ray &r, hitable *world, int depth) {
 
 int main(int argc, char const *argv[]) {
 	fileIO();
-	int nx = 200;
-	int ny = 100;
+	int nx = 400;
+	int ny = 200;
 	int ns = 100;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	hitable *list[2];
+	hitable *list[4];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambert(vec3(0.8, 0.3, 0.3)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambert(vec3(0.8, 0.8, 0.0)));
-	hitable *world = new hitable_list(list, 2);
+	list[2] = new sphere(vec3(1, -0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.0));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+	//list[4] = new sphere(vec3(-1, 0, -1), -0.475, new dielectric(1.5));
+	hitable *world = new hitable_list(list, 4);
 	camera cam;
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
@@ -67,7 +56,7 @@ int main(int argc, char const *argv[]) {
 			}
 
 			col /= float(ns);
-			col = vec3(Sqrt(col[0]), Sqrt(col[1]), Sqrt(col[2]));
+			col = vec3(almostSqrt(col[0]), almostSqrt(col[1]), almostSqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
--- material.h
+++ material.h
@@ -6,7 +6,29 @@ struct hit_record;
 #include "ray.h"
 #include "hitable.h"
 
-uint32_t xor128(void) {
+
+inline float invSqrt(float x) {
+	float xhalf = 0.5f * x;
+	int i = *(int *)&x;				// store floating-point bits in integer
+	i = 0x5f3759df - (i >> 1);		// initial guess for Newton's method
+	x = *(float *)&i;				// convert new bits into float
+	x = x * (1.5f - xhalf * x * x); // One round of Newton's method
+	return x;
+}
+
+float almostSqrt(float x) {
+	return 1 / invSqrt(x);
+}
+
+inline float schlick(float c, float ref_idx) {
+	float r0 = (1-ref_idx) / (1+ref_idx);
+	r0 *= r0;
+	c = 1-c;
+	float cc = c*c;
+	return r0 + (1-r0) * c * cc * cc;
+}
+
+inline uint32_t xor128(void) {
 	static uint32_t x = 123456789;
 	static uint32_t y = 362436069;
 	static uint32_t z = 521288629;
@@ -35,6 +57,18 @@ vec3 reflect(const vec3 &v, const vec3 &n) {
 	return v - 2 * dot(v, n) * n;
 }
 
+bool refract(const vec3 &v, const vec3 &n, float ni_over_nt, vec3 &refracted) {
+	vec3 uv = unit_vector(v);
+	float dt = dot(uv, n);
+	float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1-dt*dt);
+
+	if(discriminant > 0) {
+		refracted = ni_over_nt*(uv - n*dt) - n*almostSqrt(discriminant);
+		return true;
+	} else 
+		return false;
+}
+
 class material  {
   public:
 	virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const = 0;
@@ -43,6 +77,7 @@ class material  {
 class lambert : public material {
   public:
 	lambert(const vec3 &a) : albedo(a) {}
+
 	virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const  {
 		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
 		scattered = ray(rec.p, target - rec.p);
@@ -59,14 +94,57 @@ class metal : public material {
 		if (f < 1) fuzz = f;
 		else fuzz = 1;
 	}
+
 	virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const  {
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
 		scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
+
 	vec3 albedo;
 	float fuzz;
+};
+
+class dielectric : public material {
+  public:
+	dielectric(float ri) : ref_idx(ri) {}
+
+	virtual bool scatter(const ray &r_in, const hit_record &rec, vec3 &attenuation, ray &scattered) const  {
+		vec3 outward_normal;
+		vec3 reflected = reflect(r_in.direction(), rec.normal);
+		float ni_over_nt;
+		attenuation = vec3(1.0, 1.0, 1.0);
+		vec3 refracted;
+		float reflect_prob;
+		float cos;
+
+		if(dot(r_in.direction(), rec.normal) > 0) { 
+			outward_normal = -rec.normal;
+			ni_over_nt = ref_idx;
+			cos = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length(); //???? // is length one?
+		} else {
+			outward_normal = rec.normal;
+			ni_over_nt = 1.0 / ref_idx;
+			cos = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+		}
+
+		if(refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+			reflect_prob = schlick(cos, ref_idx);
+		} else {
+			reflect_prob = 1.0;
+		}
+
+		if(random() < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		} else {
+			scattered = ray(rec.p, refracted);
+		}
+
+		return true;
+	}
+
+	float ref_idx;
 };
 
 #endif
