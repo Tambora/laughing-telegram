a3f14642c5ab8b3a9cfeb23028c53c66552da0a6
Rohan Behl <www.rbehl.com>
2019-07-03 15:24:45 -0700

    Structuring

--- main.cpp
+++ main.cpp
@@ -6,14 +6,8 @@
 #include "float.h"
 #include "camera.h"
 #include "material.h"
+#include "bitmap.h"
 
-const int bytesPerPixel = 3; /// red, green, blue
-const int fileHeaderSize = 14;
-const int infoHeaderSize = 40;
-
-void generateBitmapImage(unsigned char *image, int height, int width, int pitch, const char* imageFileName);
-unsigned char* createBitmapFileHeader(int height, int width, int pitch, int paddingSize);
-unsigned char* createBitmapInfoHeader(int height, int width);
 
 
 vec3 color(const ray &r, hitable *world, int depth) {
@@ -61,13 +55,10 @@ hitable *random_scene() {
 }
 
 int main(int argc, char const *argv[]) {
-	int nx = 320;
-	int ny = 180;
-	int ns = 10;
 
-
-    int w = nx; /* Put here what ever width you want */
-    int h = ny; /* Put here what ever height you want */
+    int w = 320; // width
+    int h = 180; // height
+	int ns = 10; // samples
     int red[w][h]; 
     int green[w][h];
     int blue[w][h];
@@ -80,14 +71,14 @@ int main(int argc, char const *argv[]) {
     vec3 lookat(0,0,0);
 	float dist_to_focus = 10;
 	float aperture = 0.1;
-	camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
-	for (int j = ny - 1; j >= 0; j--) {
-		for (int i = 0; i < nx; i++) {
+	camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(w)/float(h), aperture, dist_to_focus);
+	for (int j = h - 1; j >= 0; j--) {
+		for (int i = 0; i < w; i++) {
 			vec3 col(0, 0, 0);
 
 			for (int s = 0; s < ns; s++) {
-				float u = float(i + random()) / float(nx);
-				float v = float(j + random()) / float(ny);
+				float u = float(i + random()) / float(w);
+				float v = float(j + random()) / float(h);
 				ray r = cam.get_ray(u, v);
 				vec3 p = r.point_at_parameter(2.0);
 				col += color(r, world, 0);
@@ -98,89 +89,12 @@ int main(int argc, char const *argv[]) {
 			img[j][i][2] = (unsigned char)(int)(255 * col[0]);
 			img[j][i][1] = (unsigned char)(int)(255 * col[1]);
 			img[j][i][0] = (unsigned char)(int)(255 * col[2]);
-
-
 		}
 	}
 
 	generateBitmapImage((unsigned char *)img, h, w, bytesPerPixel*w, "img.bmp");
-
 	return 0;
 }
-
-void generateBitmapImage(unsigned char *image, int height, int width, int pitch, const char* imageFileName) {
-
-    unsigned char padding[3] = { 0, 0, 0 };
-    int paddingSize = (4 - (/*width*bytesPerPixel*/ pitch) % 4) % 4;
-
-    unsigned char* fileHeader = createBitmapFileHeader(height, width, pitch, paddingSize);
-    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
-    FILE* imageFile = fopen(imageFileName, "wb");
-
-    fwrite(fileHeader, 1, fileHeaderSize, imageFile);
-    fwrite(infoHeader, 1, infoHeaderSize, imageFile);
-
-    int i;
-    for (i = 0; i < height; i++) {
-        fwrite(image + (i*pitch /*width*bytesPerPixel*/), bytesPerPixel, width, imageFile);
-        fwrite(padding, 1, paddingSize, imageFile);
-    }
-
-    fclose(imageFile);
-}
-
-unsigned char* createBitmapFileHeader(int height, int width, int pitch, int paddingSize) {
-    int fileSize = fileHeaderSize + infoHeaderSize + (/*bytesPerPixel*width*/pitch + paddingSize) * height;
-
-    static unsigned char fileHeader[] = {
-        0,0, /// signature
-        0,0,0,0, /// image file size in bytes
-        0,0,0,0, /// reserved
-        0,0,0,0, /// start of pixel array
-    };
-
-    fileHeader[0] = (unsigned char)('B');
-    fileHeader[1] = (unsigned char)('M');
-    fileHeader[2] = (unsigned char)(fileSize);
-    fileHeader[3] = (unsigned char)(fileSize >> 8);
-    fileHeader[4] = (unsigned char)(fileSize >> 16);
-    fileHeader[5] = (unsigned char)(fileSize >> 24);
-    fileHeader[10] = (unsigned char)(fileHeaderSize + infoHeaderSize);
-
-    return fileHeader;
-}
-
-unsigned char* createBitmapInfoHeader(int height, int width) {
-    static unsigned char infoHeader[] = {
-        0,0,0,0, /// header size
-        0,0,0,0, /// image width
-        0,0,0,0, /// image height
-        0,0, /// number of color planes
-        0,0, /// bits per pixel
-        0,0,0,0, /// compression
-        0,0,0,0, /// image size
-        0,0,0,0, /// horizontal resolution
-        0,0,0,0, /// vertical resolution
-        0,0,0,0, /// colors in color table
-        0,0,0,0, /// important color count
-    };
-
-    infoHeader[0] = (unsigned char)(infoHeaderSize);
-    infoHeader[4] = (unsigned char)(width);
-    infoHeader[5] = (unsigned char)(width >> 8);
-    infoHeader[6] = (unsigned char)(width >> 16);
-    infoHeader[7] = (unsigned char)(width >> 24);
-    infoHeader[8] = (unsigned char)(height);
-    infoHeader[9] = (unsigned char)(height >> 8);
-    infoHeader[10] = (unsigned char)(height >> 16);
-    infoHeader[11] = (unsigned char)(height >> 24);
-    infoHeader[12] = (unsigned char)(1);
-    infoHeader[14] = (unsigned char)(bytesPerPixel * 8);
-
-    return infoHeader;
-}
-	//std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-	//std::cout << ir << " " << ig << " " << ib << "\n";
 
 // x*x + 0.2*(x*x*x - x*x)
 // 0.2*x*x + 0.8*x*x*x
