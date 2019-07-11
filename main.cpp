41190ef4f02d6dc0f95cd3d709ec448b9ca08784
Rohan Behl <www.rbehl.com>
2019-07-11 01:54:26 -0700

    Fixed small error with box normals

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ hitable *cornell_box() {
     list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
     list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130,0,65));
     list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white),  15), vec3(265,0,295));
-	return new bvh_node(list, i);
+	return new hitable_list(list, i);
 }
 
 
